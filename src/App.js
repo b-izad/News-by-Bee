68a1794d84385c863f49eb7cb0a3792282468ffd
Bahareh <izad@bu.edu>
2023-12-12 13:38:39 -0500

    made the adjustment to hide the buttons on small screens

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
-import React , {useState} from 'react';
+import React , {useState ,useEffect} from 'react';
+import './App.css'
 import Header from './Header';
 import NewsList from './NewsList';
 
+
+
 function App() {
   const [articles, setArticles] = useState([]);
-  const [visibleCount, setVisibleCount] = useState(3); // State to track the number of visible articles
+  const [visibleCount, setVisibleCount] = useState(3); 
+  const [topHeadlines, setTopHeadlines] = useState([]);
 
-  // ... existing onSelectCategory and other functions ...
+  useEffect(() => {
+      fetch(`https://newsapi.org/v2/top-headlines?country=us&apiKey=a8b244d7540e4b998c5fd2d6f00128aa`)
+      .then(response => response.json())
+      .then(data => setTopHeadlines(data.articles))
+      .catch(error => console.error('Error fetching top headlines:', error));
+  }, []);
+  
 
   const showMoreArticles = () => {
     setVisibleCount(prevCount => prevCount + 3); // Increase the count by 3 each time 'Show More' is clicked
@@ -39,12 +49,14 @@ function App() {
 
 
   return (
-    <div>
+    <div className="app-container">
+            <div className="main-content">
       <Header onSelectCategory={onSelectCategory} />
       <NewsList articles={articles.slice(0, visibleCount)} /> {/* Only display a slice of articles */}
       {visibleCount < articles.length && (
-        <button onClick={showMoreArticles}>Show More</button> // Show this button only if there are more articles to show
+        <button onClick={showMoreArticles} className="show-more-button">Show More</button> // Show this button only if there are more articles to show
       )}
+    </div>
     </div>
   );
 }
